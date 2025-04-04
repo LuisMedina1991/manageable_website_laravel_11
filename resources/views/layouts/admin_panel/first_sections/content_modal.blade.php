6eb489a056fb3dba40666644f60eeb7175c4d2d9
LuisMedina1991 <luchito19medina91@gmail.com>
2025-04-04 12:36:45 -0400

    Round first section frames - resolves #1

--- resources/views/layouts/admin_panel/first_sections/content_modal.blade.php
+++ resources/views/layouts/admin_panel/first_sections/content_modal.blade.php
@@ -9,16 +9,14 @@
                 <div class="text-center fs-3 fw-bold {{$first_section->textColor->class}}">
                     <div class="container">
                         <span class="text-uppercase">{{$first_section->title}}</span>
-                        @foreach($first_section->firstSectionFrames as $first_section_frame)
-                            <div class="row pt-3 g-0">
-                                <div class="card border-4 border-dark rounded-5">
-                                    <div class="card-header border-bottom border-4 border-dark">{{ $first_section_frame->subtitle }}</div>
-                                    <div class="card-body">
-                                            <span class="card-text">{{ $first_section_frame->text }}</span>
-                                    </div>
+                        <div class="row pt-3 g-3">
+                            @foreach($first_section->firstSectionFrames as $first_section_frame)
+                                <div class="col-12 border border-4 border-dark rounded-5">
+                                    <span class="d-block border-bottom border-4 border-dark">{{ $first_section_frame->subtitle }}</span>
+                                    <span>{{ $first_section_frame->text }}</span>
                                 </div>
-                            </div>
-                        @endforeach
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
